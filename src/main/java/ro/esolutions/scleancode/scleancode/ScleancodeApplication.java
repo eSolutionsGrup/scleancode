671dbeb1493a6925df7e57858b23ac6d44747945
vborcea <borceavirgil@gmail.com>
2018-08-08 17:45:04 +0300

    first commit

---
+++ src/main/java/ro/esolutions/scleancode/scleancode/ScleancodeApplication.java
@@ -0,0 +1,12 @@
+package ro.esolutions.scleancode.scleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScleancodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScleancodeApplication.class, args);
+	}
+}
